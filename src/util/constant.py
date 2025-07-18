69a0584be5e9d3f7b0a2127caea189a7359f1949
gglim <dlarkqrl4966@gmail.com>
2025-07-18 19:38:10 +0900

    Refactor constant definitions in Gitoy utility module

--- src/util/constant.py
+++ src/util/constant.py
@@ -1,7 +1,7 @@
+from enum import Enum
+
 GITOY_DIR = '.gitoy'
 GITOY_DB_FILE = 'gitoy.db'
-
-from enum import Enum
 
 class GitoyMessage(Enum):
     REPOSITORY_ALREADY_INITIALIZED = 'Gitoy Repository already initialized'
