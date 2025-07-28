45ab54cc9b49583f89f75950b22a33002701f28c
gglim <dlarkqrl4966@gmail.com>
2025-07-29 08:43:34 +0900

    Update test cases to include relative paths for index entry listing

--- test/database/test_database.py
+++ test/database/test_database.py
@@ -40,31 +40,34 @@ class TestDatabase:
     def test_list_index_entries_by_paths_startwith(self, database: Database):
         database.create_index_entries(
             [
-                self.random_index_entry('file.txt'),
-                self.random_index_entry('test/file1.txt'),
-                self.random_index_entry('test/test2/file2.txt'),
-                self.random_index_entry('test/test2/test3/file3.txt'),
-                self.random_index_entry('test/test2/test3/file4.txt'),
+                self.random_index_entry('./file.txt'),
+                self.random_index_entry('./test/file1.txt'),
+                self.random_index_entry('./test/test2/file2.txt'),
+                self.random_index_entry('./test/test2/test3/file3.txt'),
+                self.random_index_entry('./test/test2/test3/file4.txt'),
             ]
         )
 
         entries = database.list_index_entries()
         assert len(entries) == 5
 
-        entries = database.list_index_entries_by_paths_startwith(['test/file1.txt', 'test/test2/file2.txt'])
+        entries = database.list_index_entries_by_paths_startwith(['./'])
+        assert len(entries) == 5
+
+        entries = database.list_index_entries_by_paths_startwith(['./test/file1.txt', './test/test2/file2.txt'])
         assert len(entries) == 2
 
-        entries = database.list_index_entries_by_paths_startwith(['test/'])
+        entries = database.list_index_entries_by_paths_startwith(['./test'])
         assert len(entries) == 4
 
-        entries = database.list_index_entries_by_paths_startwith(['test/test2'])
+        entries = database.list_index_entries_by_paths_startwith(['./test/test2'])
         assert len(entries) == 3
 
         entries = database.list_index_entries_by_paths_startwith(['test2'])
         assert len(entries) == 0
 
-        entries = database.list_index_entries_by_paths_startwith(['test/test2/test3'])
+        entries = database.list_index_entries_by_paths_startwith(['./test/test2/test3'])
         assert len(entries) == 2
 
-        entries = database.list_index_entries_by_paths_startwith(['test/test2/test3', 'test'])
+        entries = database.list_index_entries_by_paths_startwith(['./test/test2/test3', './test'])
         assert len(entries) == 4
